2101baf93f22da20b67186bfd9fe21b01431e5b3
Rajan Chauhan <rajan-c@PROD.HCLPNP.COM>
2018-04-08 19:34:51 +0530

    adding microservice-config-server microservice.

---
+++ microservice-config-server/src/main/java/com/rajan/configServer/MicroserviceConfigServerApplication.java
@@ -0,0 +1,12 @@
+package com.rajan.configServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceConfigServerApplication.class, args);
+	}
+}
